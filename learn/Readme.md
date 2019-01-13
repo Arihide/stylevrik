16e9760db96bab022e4270c5bb22b453e1f26294
a.takahashi <arihide.takahashi@gmail.com>
2019-01-13 22:55:42 +0900

    新モデル　悪くない

--- learn/Readme.md
+++ learn/Readme.md
@@ -4,3 +4,11 @@
     - python3.6
     - GPy (ガウス過程ライブラリ)
     - mathutils (3DCGに特化した計算ライブラリ)
+    - plotly
+
+
+### mathutilの導入
+
+pipや本家のblender-mathutilsからではうまくいかなかった。
+https://gitlab.com/m1lhaus/blender-mathutils からダウンロードする。
+
--- learn/hoge2.py
+++ learn/hoge2.py
@@ -1,23 +1,29 @@
 import numpy as np
+import sys
 from bvh_reader import BVHReader
 
 if __name__ == '__main__':
+
+    N = 130
 
     # extract_indices = [
     #     957, 967, 1146, 2763
     # ]
     
     extract_indices = np.hstack((
-        np.linspace(957, 2763, 100).astype(np.int),
-        np.linspace(3502, 4899, 100).astype(np.int)
+        np.linspace(957, 2763, N).astype(np.int),
+        np.linspace(3502, 4899, N).astype(np.int)
     ))
 
     br = BVHReader('bvh/Scene_5_Char00_rmfinger.bvh')
     br.read()
-    
+
+    # if sys.argv[0] is not None:
+    #     sys.exit()
+
     Y = np.asarray(br.motions)
 
-    with open('bvh/Scene_5_rmfinger_reduced.bvh', 'w') as ftarget:
+    with open('bvh/Scene_5_rmfinger_reduced2.bvh', 'w') as ftarget:
 
         ftarget.write('Frames: %d\n' % extract_indices.size)
         ftarget.write('Frame Time: %f\n' % 0.01)
